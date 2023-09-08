0d8a4517c776dd0cd0a0a7d4046e4027d134fca1
atiilla <9992685+atiilla@users.noreply.github.com>
2023-09-08 21:49:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# GitHub Email Scanner
+
+A tool to scan GitHub repositories for exposed email addresses and names.
+
+## Introduction
+
+This tool uses the GitHub API to scan repositories owned by a user for email addresses and associated names. It provides options to scan specific repositories and exclude forked repositories. Additionally, you can provide a GitHub API token to increase the rate limit for API requests.
+
+### Installation
+
+```
+npm install -g gitrecon
+```
+
+### Usage
+Command Line Arguments:
+```
+  -u, --username <username>         GitHub username (Required)
+  -t, --token <token>               GitHub API token (Optional)
+  -r, --repo <repo>                 Repository name (Optional)
+  -n, --no-forks                    Exclude forked repositories (Optional)
+  -h, --help                        output usage information
+```
+
+Example usage:
+Scan all public repositories of a user:
+```
+gitrecon -u <username> -n
+```
+
+Scan a specific repository of a user:
+```
+gitrecon -u <username> -r <repo>
+```
+
+Provide a GitHub API token:
+```
+gitrecon -u <username> -t <token>
+```
+
+## Implementation Details
+
+### Constants
+`API_URL:` The base URL for GitHub API.
+
+`HEADER:` Default HTTP headers for API requests.
+
+`DELAY:` Delay between API requests (3000 milliseconds).
+
+### Functions
+`Repository(name, isFork):` Factory function to create Repository objects.
+
+`updateHeader(updateObj):` Function to update HTTP headers.
+
+`getRepositories(username):` Function to retrieve user's repositories.
+
+`getEmails(username, repoName):` Function to retrieve email addresses from a repository's commits.
+
+`apiCall(url):` Function to make API calls with a delay.
+
+### Main Function
+* Parses command line arguments.
+* Fetches repositories to scan based on user input.
+* Scans repositories for email addresses and names.
+* Displays the results.
+
+## C++ version
+A C++ version of this tool is available [here](
+    c++/
+).
+
+### C++ future work
+* GUI for the tool.
+
+### `Ethics and Disclaimer`
+```
+This tool is intended to be used for educational purposes only. The author is not responsible for any misuse of this tool.
+```
---
+++ index.js
@@ -0,0 +1,266 @@
+#!/usr/bin/env node
+// Import required modules
+const axios = require('axios');
+const { ArgumentParser } = require('argparse');
+
+const colors = {
+        GREEN: '\x1b[32m',
+        YELLOW: '\x1b[33m',
+        NC: '\x1b[0m',
+        CYAN: '\x1b[36m',
+    }
+
+// Constants
+const API_URL = 'https://api.github.com';
+const HEADER = {
+    Accept: 'application/vnd.github.v3+json',
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36',
+};
+
+let DELAY = 3000; // Delay of one second between requests
+
+// Factory function to create Repository objects
+const Repository = (name, isFork) => ({
+    name,
+    isFork,
+});
+
+// Function to update HTTP headers
+const updateHeader = (updateObj) => {
+    Object.assign(HEADER, updateObj);
+};
+
+// Function to retrieve user's repositories
+const getRepositories = async (username) => {
+    const repositoriesSeen = new Set();
+    const repositories = [];
+    let pageCounter = 1;
+
+    while (true) {
+        let continueLoop = true;
+
+        // Construct the URL for fetching repositories
+        const url = `${API_URL}/users/${username}/repos?per_page=100&page=${pageCounter}`;
+        const result = await apiCall(url);
+
+        if ('message' in result) {
+            if (result.message.includes('API rate limit exceeded for ')) {
+                console.warn('API rate limit exceeded - not all repos were fetched');
+                break;
+            }
+            if (result.message === 'Not Found') {
+                console.warn(`There is no user with the username "${username}"`);
+                break;
+            }
+        }
+
+        // Process each repository in the result
+        for (const repository of result) {
+            const repoName = repository.name;
+            if (repositoriesSeen.has(repoName)) {
+                continueLoop = false;
+                break;
+            } else {
+                repositories.push(Repository(repoName, repository.fork));
+                repositoriesSeen.add(repoName);
+            }
+        }
+
+        if (continueLoop && result.length === 100) {
+            pageCounter += 1;
+        } else {
+            break;
+        }
+    }
+
+    return repositories;
+};
+
+// Function to retrieve email addresses from a repository's commits
+const getEmails = async (username, repoName) => {
+    const emailsToName = new Map();
+    const seenCommits = new Set();
+    let pageCounter = 1;
+    let commitCounter = 1;
+
+    while (true) {
+        let continueLoop = true;
+        const url = `${API_URL}/repos/${username}/${repoName}/commits?per_page=100&page=${pageCounter}`;
+        const result = await apiCall(url);
+
+        if ('message' in result) {
+            if (result.message === 'Git Repository is empty.') {
+                console.info('Git repository is empty');
+                continue;
+            }
+
+            if (result.message.includes('API rate limit exceeded for ')) {
+                console.warn('API rate limit exceeded');
+                return emailsToName;
+            }
+
+            if (result.message === 'Not Found') {
+                console.warn(`Repository Not Found: "${repoName}"`);
+                return emailsToName;
+            }
+        }
+
+        // Process each commit in the result
+        for (const commit of result) {
+            const sha = commit.sha;
+            if (seenCommits.has(sha)) {
+                continueLoop = false;
+                break;
+            }
+
+            seenCommits.add(sha);
+            console.info(`Scanning commit -> ${commitCounter}`);
+            commitCounter += 1;
+
+            if (!commit.author) {
+                continue;
+            }
+            const user = commit.author.login;
+            if (user.toLowerCase() === username.toLowerCase()) {
+                const { author, committer } = commit.commit;
+                const authorName = author.name;
+                const authorEmail = author.email;
+                const committerName = committer.name;
+                const committerEmail = committer.email;
+                if (authorEmail) {
+                    if (!emailsToName.has(authorEmail)) {
+                        emailsToName.set(authorEmail, new Set());
+                    }
+                    emailsToName.get(authorEmail).add(authorName);
+                }
+                if (committerEmail) {
+                    if (!emailsToName.has(committerEmail)) {
+                        emailsToName.set(committerEmail, new Set());
+                    }
+                    emailsToName.get(committerEmail).add(committerName);
+                }
+            }
+        }
+
+        if (continueLoop && result.length === 100) {
+            pageCounter += 1;
+        } else {
+            break;
+        }
+    }
+
+    return emailsToName;
+};
+
+// Function to make API calls with a delay
+const apiCall = async (url) => {
+    await new Promise((resolve) => setTimeout(resolve, DELAY));
+    const response = await axios.get(url, { headers: HEADER, timeout: 10000 });
+    return response.data;
+};
+
+// Main function
+const main = async () => {
+    console.log(`${colors.CYAN}
+    ██████╗ ██╗████████╗██████╗ ███████╗ ██████╗ ██████╗ ███╗   ██╗
+    ██╔════╝ ██║╚══██╔══╝██╔══██╗██╔════╝██╔════╝██╔═══██╗████╗  ██║
+    ██║  ███╗██║   ██║   ██████╔╝█████╗  ██║     ██║   ██║██╔██╗ ██║
+    ██║   ██║██║   ██║   ██╔══██╗██╔══╝  ██║     ██║   ██║██║╚██╗██║
+    ╚██████╔╝██║   ██║   ██║  ██║███████╗╚██████╗╚██████╔╝██║ ╚████║
+     ╚═════╝ ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝
+                                            https://github.com/atiilla
+    `);
+
+    // Create an argument parser
+    const parser = new ArgumentParser({
+        add_help: true, // Use add_help instead of addHelp
+        description:
+            'A tool to scan GitHub repositories for exposed email addresses and names',
+    });
+
+    // Define command line arguments
+    parser.add_argument('-u', '--user', {
+        help: 'name of the user whose repositories should be scanned',
+        type: String,
+        required: true,
+    });
+
+    parser.add_argument('-r', '--repository', {
+        help: 'name of the repository which should be scanned',
+        type: String,
+    });
+
+    parser.add_argument('-t', '--token', {
+        help: 'GitHub API token (optional) to increase the rate limit',
+        type: String,
+    });
+
+    parser.add_argument('-n', '--no-forks', {
+        help: 'do not scan forked repositories',
+        action: 'store_true',
+    });
+
+    // Parse command line arguments
+    const args = parser.parse_args();
+
+    if (!args.user) {
+        console.warn('No username specified');
+        parser.printHelp();
+        return;
+    }
+
+    if (args.token) {
+        updateHeader({ Authorization: `token ${args.token}` });
+    }
+
+    let reposToScan = [];
+
+    if (args.repository) {
+        reposToScan = [args.repository];
+    } else {
+        console.info(`Scan all public repositories of ${args.user}`);
+        const reposToScanSorted = (
+            await getRepositories(args.user)
+        ).sort((a, b) => (a.isFork ? 1 : -1));
+        reposToScan = reposToScanSorted
+            .filter(
+                (repo) =>
+                    !args.no_forks || !repo.isFork
+            )
+            .map((repo) => repo.name);
+        console.info(`Found ${reposToScan.length} public repositories`);
+    }
+
+    const emailsToName = new Map();
+    try {
+        for (const repo of reposToScan) {
+            console.info(`Scan repository ${repo}`);
+            const emailsToNameNew = await getEmails(args.user, repo);
+            for (const [email, names] of emailsToNameNew.entries()) {
+                if (!emailsToName.has(email)) {
+                    emailsToName.set(email, new Set());
+                }
+                names.forEach((name) => emailsToName.get(email).add(name));
+            }
+        }
+    } catch (error) {
+        console.warn('An error occurred:', error.message);
+    }
+
+    if (emailsToName.size > 0) {
+        const maxEmailWidth = Math.max(...Array.from(emailsToName.keys(), (email) => email.length));
+        console.info(`${colors.YELLOW}Found the following emails:`);
+        for (const [email, names] of emailsToName.entries()) {
+            const namesString = Array.from(names).join('; ');
+            console.info(
+                `${colors.GREEN} ${email.padEnd(maxEmailWidth, ' ')} - ${namesString}`
+            );
+        }
+    } else {
+        console.info('No emails found');
+    }
+};
+
+// Run the main function and handle errors
+main().catch((error) => console.error(error));
